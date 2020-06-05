74d8043d72a19e093de6b2957dc04e79511ec53a
chamedi-warnasuriya <chamediwarnasuriya@gmail.com>
2020-06-05 21:39:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
-(function($){
-	$(window).on("load",function(){
-		$(".mCustomScrollbar").mCustomScrollbar();
-	});
-})(jQuery);
-
-
-$(function () {
-	$(document).ready(function(){
-		$('[data-toggle="offcanvas"]').click(function () {
-			$('#sideBarToggle').toggleClass('toggled');
-		}); 
-		$('[data-toggle="offContact"]').click(function () {
-			$('#contact').toggleClass('toggled');
-		});  
-		$('[data-toggle="offRegister"]').click(function () {
-			$('#register').toggleClass('toggled');
-		});   
-		$('[data-toggle="offPolicy"]').click(function () {
-			$('#policy').toggleClass('toggled');
-		}); 
-
-		$('[data-spy="scroll"]').on('activate.bs.scrollspy', function (evt) {
-			var step = $(evt.relatedTarget).index();
-			console.log(step);
-		})
-
-		/* START Initialize Carousel */
+/* START Carousel */
+	$(function () {
+		$(document).ready(function(){
 			$(".carousel").on('slide.bs.carousel', function(evt) {
 				var _this = $(this);
 				var step = $(evt.relatedTarget).index();
@@ -44,7 +19,6 @@ $(function () {
 			});
 			$('.carousel .carousel-control-prev').fadeOut('fast');
 
-
 			$("#carouselInclusions").on('slide.bs.carousel', function(evt) {
 				var step = $(evt.relatedTarget).index();
 				$('#slider_captions .carousel-caption:not(#caption-'+step+')').fadeOut('fast', function() {
@@ -53,43 +27,73 @@ $(function () {
 			});
 
 			$('#carouselGallery').carousel({ interval: false });
-			$('#carouselInclusions').carousel({ interval: false });
-		/* END Initialize Sliders */
-
-		$('[data-toggle="tooltip"]').tooltip();
-
+			$('#carouselInclusions').carousel({ interval: false });	
+		});
 	});
-});
+/* END Carousel */
 
-$(document).ready(function () {
- 
-});
-
-$(function () {
-	$(document).ready(function(){
-		var $animation_elements = $('.animation-element');
-		var $window = $(window);
-
-		function check_if_in_view() {
-		  var window_height = $window.height();
-		  var window_top_position = $window.scrollTop();
-		  var window_bottom_position = (window_top_position + window_height);
-		 
-		  $.each($animation_elements, function() {
-		    var $element = $(this);
-		    var element_height = $element.outerHeight();
-		    var element_top_position = $element.offset().top;
-		    var element_bottom_position = (element_top_position + element_height);
-		 
-		    //check to see if this current container is within viewport
-		    if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
-		      	$element.addClass('in-view');
-		    }
-		  });
-		}
-
-		$window.on('scroll resize', check_if_in_view);
-		$('.js-loop').on('scroll resize', check_if_in_view);
-		$window.trigger('scroll');
+/* START Sidebar-Popup */
+	$(function () {
+		$(document).ready(function(){
+			$('[data-toggle="offcanvas"]').click(function () {
+				$('#sideBarToggle').toggleClass('toggled');
+			}); 
+			$('[data-toggle="offContact"]').click(function () {
+				$('#contact').toggleClass('toggled');
+			});  
+			$('[data-toggle="offRegister"]').click(function () {
+				$('#register').toggleClass('toggled');
+			});   
+			$('[data-toggle="offPolicy"]').click(function () {
+				$('#policy').toggleClass('toggled');
+			});
+		});
 	});
-});
+/* END Sidebar-Popup */
+
+/* START Tooltip */
+	$(function () {
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip();
+		});
+	});
+/* END Tooltip */	
+
+/* START Custom Scroll */
+	(function($){
+		$(window).on("load",function(){
+			$(".mCustomScrollbar").mCustomScrollbar();
+		});
+	})(jQuery);
+/* START Custom Scroll */
+
+/* START Animation */
+	$(function () {
+		$(document).ready(function(){
+			var $animation_elements = $('.animation-element');
+			var $window = $(window);
+
+			function check_if_in_view() {
+			  var window_height = $window.height();
+			  var window_top_position = $window.scrollTop();
+			  var window_bottom_position = (window_top_position + window_height);
+			 
+			  $.each($animation_elements, function() {
+			    var $element = $(this);
+			    var element_height = $element.outerHeight();
+			    var element_top_position = $element.offset().top;
+			    var element_bottom_position = (element_top_position + element_height);
+			 
+			    //check to see if this current container is within viewport
+			    if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+			      	$element.addClass('in-view');
+			    }
+			  });
+			}
+
+			$window.on('scroll resize', check_if_in_view);
+			$('.js-loop').on('scroll resize', check_if_in_view);
+			$window.trigger('scroll');
+		});
+	});
+/* END Animation */
